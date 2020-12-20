5795d44c368ba5e4a9ea7f8732f692341ca1dece
Amira Khan <amira.zipfs@gmail.com>
2020-12-19 17:11:07 -0800

    testing: regression

--- bin/test_zipfs.py
+++ bin/test_zipfs.py
@@ -50,3 +50,13 @@ def test_integration():
     actual_alpha = plotcounts.get_power_law_params(counts_array)
     expected_alpha = pytest.approx(1.0, abs=0.01)
     assert actual_alpha == expected_alpha
+
+
+def test_regression():
+    """Regression test for Dracula."""
+    with open('data/dracula.txt', 'r') as reader:
+        word_counts_dict = countwords.count_words(reader)
+    counts_array = np.array(list(word_counts_dict.values()))
+    actual_alpha = plotcounts.get_power_law_params(counts_array)
+    expected_alpha = pytest.approx(1.087, abs=0.001)
+    assert actual_alpha == expected_alpha
