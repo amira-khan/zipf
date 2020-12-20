7b13937360e69c73468caabfef2ab513181e77cc
Amira Khan <amira.zipfs@gmail.com>
2020-12-19 20:12:59 -0800

    errors: suggested changes before exercises

--- bin/collate.py
+++ bin/collate.py
@@ -6,8 +6,14 @@ into a single cumulative count.
 import csv
 import argparse
 from collections import Counter
+import logging
 
 import utilities as util
+
+
+ERRORS = {
+    'not_csv_suffix' : '{fname}: File must end in .csv',
+    }
 
 
 def update_counts(reader, word_counts):
@@ -19,8 +25,14 @@ def update_counts(reader, word_counts):
 def main(args):
     """Run the command line program."""
     word_counts = Counter()
+    logging.info('Processing files...')
     for fname in args.infiles:
+        logging.debug(f'Reading in {fname}...')
+        if fname[-4:] != '.csv':
+            msg = ERRORS['not_csv_suffix'].format(fname=fname)
+            raise OSError(msg)
         with open(fname, 'r') as reader:
+            logging.debug('Computing word counts...')
             update_counts(reader, word_counts)
     util.collection_to_csv(word_counts, num=args.num)
 
@@ -34,3 +46,4 @@ if __name__ == '__main__':
                         help='Output only n most frequent words')
     args = parser.parse_args()
     main(args)
+    
