aa09b73a4dd3087cead388de6aa00bbfea10b597
Amira Khan <amira.zipfs@gmail.com>
2020-12-19 16:42:14 -0800

    config exercise: plotting styles

--- bin/plotcounts.py
+++ bin/plotcounts.py
@@ -6,6 +6,7 @@ import yaml
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
+import matplotlib.pyplot as plt
 from scipy.optimize import minimize_scalar
 
 
@@ -76,6 +77,8 @@ def plot_fit(curve_xmin, curve_xmax, max_rank, alpha, ax):
 
 def main(args):
     """Run the command line program."""
+    if args.style:
+        plt.style.use(args.style)
     set_plot_params(args.plotparams)
     df = pd.read_csv(args.infile, header=None,
                      names=('word', 'word_frequency'))
@@ -117,5 +120,8 @@ if __name__ == '__main__':
                         default=None, help='X-axis limits')
     parser.add_argument('--plotparams', type=str, default=None,
                         help='matplotlib parameters (YAML file)')
+    parser.add_argument('--style', type=str, nargs='*',
+                        choices=plt.style.available,
+                        default=None, help='matplotlib style')
     args = parser.parse_args()
     main(args)
